34cd84ba715b2e44b03773682e1f1bcf26db902b
Stuart <sjmcpherso@gmail.com>
2013-05-27 18:01:50 +1200

    Attempt v1

---
+++ testmenu.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<!--[if (lt IE 8)]>      <html class="no-js lt-ie9 lt-ie8 wf-loading"> <![endif]-->
+<!--[if (IE 8)]>         <html class="no-js lt-ie9 wf-loading"> <![endif]-->
+<!--[if (gt IE 8)]><!--> <html class="no-js wf-loading">
+<!--<![endif]-->
+    <head>
+    	<script type="text/javascript">WebFontConfig = {typekit: { id:'xwt5ilg' }};(function() {var wf = document.createElement('script');wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';wf.type = 'text/javascript';wf.async = 'true';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(wf, s);})();</script>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="Portfolio of Stuart McPherson">
+    <title>Portfolio of Front-End Developer Stuart McPherson</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">              
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link href="css/sjmcpherson.css" rel="stylesheet">
+        <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+     
+<body>
+<!--[if lte IE 7]><p class="ieWarning">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p><![endif]-->
+<div id="outer-wrap" class="outer-wrap">
+<div id="inner-wrap" class="inner-wrap">
+<header>
+<div class="wrap top">
+<a class="nav-btn" id="nav-open-btn" href="#nav">Book Navigation</a>
+<!--<div class="logo">
+		<a href="/">Stuart McPherson</a>
+		<h5>Front End Web Developer</h5>
+			
+    <ul class="icons">
+        <li><a href="mailto:me@sjmcpherson.com" class="fs1" aria-hidden="true" data-icon="&#xe00f;"></a></li>
+        <li><a href="nz.linkedin.com/in/stuartmcpherson" aria-hidden="true" target="_blank" data-icon="&#xe002;"></a></li>
+        <li><a href="https://twitter.com/sjmcpherso" aria-hidden="true" target="_blank" data-icon="&#xe001;"></a></li>
+    </ul></div>-->
+        </div>
+        
+<div class="wrap navedit">
+    <nav id="nav" role="navigation" class="block">
+        <ul class="menu block">
+            <li><a href="index.php">my work</a></li>
+            <li><a href="index.php#about">about me</a></li>
+            <li><a href="index.php#contact">say hello</a></li>
+        </ul>
+        <a class="close-btn" id="nav-close-btn" href="#top">Return to Content</a>
+ </nav>
+</div>
+
+</header>    
+
+
+
+<section class="wrap main">
+<a name="portfolio" id="portfolio"></a>
+            	<p class="lge">Welcome,</p> 
+                <h2>My name is Stuart & I build websites & web apps that are a pleasure to use....</h2>          
+
+                
+<div id="portlinks" class="portfolio">
+
+                  <article class="port">
+                    	<a href="diff.php"><img src="img/port-diff.jpg" alt="The Difference" /></a>                    
+                        <div class="header">
+                            <h4>Designer/Developer</h4>
+                            <h5><a href="diff.php">The Difference Catering Website</a></h5>
+                        </div>
+                    </article>  
+                    <article class="port">
+                   		<a href="icoach.php"><img src="img/port-icoach.jpg" alt="iCoach Rugby" /></a>                    
+                        <div class="header">
+                            <h4>Designer/Front-End Developer</h4>
+                            <h5><a href="icoach.php">iCoach Rugby App/Tool</a></h5>
+                        </div>
+                    </article>
+                     <article class="port">
+                   		 <a href="surveys.php"><img src="img/port-survey.jpg" alt="Airport Survey" /></a>
+                         <div class="header">
+                            <h4>Designer/Developer</h4>
+                            <h5><a href="surveys.php">Mobile Feedback Systems</a></h5>
+                        </div>
+                    </article>                     
+                    <article class="port">
+                        <a href="infield.php"><img src="img/port-infield.jpg" alt="Infield" /></a>                     
+                        <div class="header">
+                            <h4>Designer/Developer</h4>
+                            <h5><a href="infield.php">Infield Marketing Website</a></h5>
+                        </div>
+                    </article>
+                    <article class="port">
+                    	<a href="email.php"><img src="img/port-email.jpg" alt="Flipmind Email" /></a>                    
+                        <div class="header">
+                            <h4>Designer/Developer</h4>
+                            <h5><a href="email.php">Flipmind Email Campaign</a></h5>
+                        </div>
+                    </article>
+                   <article class="port">
+                        <a href="airport.php"><img src="img/port-airport.jpg" alt="Airport" /></a>                   
+                        <div class="header">
+                            <h4>Developer</h4>
+                            <h5><a href="airport.php">Auckland Airport Mobile Systems</a></h5>
+                        </div>
+                    </article>               
+                    <article class="port">
+                        <a href="flipmind.php"><img src="img/port-flipmind.jpg" alt="Flipmind" /></a>                    
+                        <div class="header">
+                            <h4>Designer/Developer</h4>
+                            <h5><a href="flipmind.php">Flipmind Business Marketing Hub</a></h5>
+                        </div>
+                    </article>
+    
+                    <article class="port">
+                   		 <a href="houseco.php"><img src="img/port-house.jpg" alt="The House Co" /></a>                    
+                        <div class="header">
+                            <h4>Designer/Developer</h4>
+                            <h5><a href="houseco.php">The House Company Website</a></h5>
+                        </div>
+                    </article>
+                    
+
+
+                    <article class="port">
+                    	<a href="dave.php"><img src="img/port-dave.jpg" alt="Dave Van Rijk" /></a>                    
+                        <div class="header">
+                            <h4>Developer</h4>
+                            <h5><a href="dave.php">Dave Van Rijk E-Commerce Site</a></h5>
+                        </div>
+                    </article>
+  
+                </div>
+           
+</section>               
+<a name="about" id="about"></a>                
+<section class="wrap main">
+             	<div class="breadcrumb"><h3>about me</h3></div>
+
+                <p class="lge">Hello,</p> 
+                <h2>I'm a Front-End Web Developer specializing in HTML5, CSS3 and JavaScript</h2>      
+                <div class="half technical">
+                <h3>Technical Skills</h3>
+                <hr class="light" />
+                <div class="graph">
+                <div class="cube eight">
+                    <div class="topFace"><div></div></div>
+                    <div class="leftFace"></div>
+                    <div class="rightFace"><p>.NET/PHP</p></div>				
+                </div>
+                <div class="cube seven l2">
+                    <div class="topFace"><div></div></div>
+                    <div class="leftFace"></div>
+                    <div class="rightFace"><p>Database Architecure</p></div>				
+                </div>
+                <div class="cube six l2">
+                    <div class="topFace"><div></div></div>
+                    <div class="leftFace"></div>
+                    <div class="rightFace"><p>Umbraco/Drupal</p></div>				
+                </div>
+                <div class="cube five l2">
+                    <div class="topFace"><div></div></div>
+                    <div class="leftFace"></div>
+                    <div class="rightFace"><p>Photoshop</p></div>				
+                </div>
+                <div class="cube four l3">
+                    <div class="topFace"><div></div></div>
+                    <div class="leftFace"></div>
+                    <div class="rightFace"><p>JavaScript/AJAX</p></div>				
+                </div>
+                <div class="cube three l3">
+                    <div class="topFace"><div></div></div>
+                    <div class="leftFace"></div>
+                    <div class="rightFace"><p>jQuery</p></div>				
+                </div>
+                <div class="cube two l4">
+                    <div class="topFace"><div></div></div>
+                    <div class="leftFace"></div>
+                    <div class="rightFace"><p>CSS/CSS3</p></div>				
+                </div>
+                <div class="cube l4">
+                    <div class="topFace"><div></div></div>
+                    <div class="leftFace"></div>
+                    <div class="rightFace"><p>HTML/HTML5</p></div>	
+                </div>
+                <div class="scale l4">                	
+                    <ul><li><span>Jedi Master</span></li><li><span>Jedi Knight</span></li><li><span>Padawan</span></li><li class="endf"><span>Youngling</span></li></ul>
+                </div>				
+                </div>
+                <!--
+                 <ul class="graph">
+                    <li class="step5">HTML/HTML5</li>
+                    <li class="step5">CSS/CSS3</li>
+                    <li class="step5">JQuery/JQuery Mobile</li>
+                    <li class="step4">JavaScript/AJAX</li>
+                    <li class="step4">Photoshop/Illustrator</li>
+                    <li class="step3">Umbraco CMS</li>
+                    <li class="step3">Titanium Appcelerator</li>  
+                    <li class="step2">SQL Server/Azure</li>    
+                    <li class="step2">.NET/PHP</li>
+                </ul>
+                <div class="base">
+                    <ul><li><span>Amature</span></li><li><span>Apprentice</span></li><li><span> Expert</span></li><li class="endf"><span>Master</span></li></ul>
+                </div> -->                   
+            </div>
+           	<article class="half form home">
+            <h3>Bio</h3>
+				<hr class="light" />
+                <p>Hi my name is Stuart McPherson my specialties are HTML5, CSS3 and JavaScript, I love developing websites with a clean and inventive user experience. While I am no fully skilled back-end developer I do have experience in Server-side .NET &amp; PHP development and even once worked as a Graphic Designer.</p>                        
+                <p>After obtaining a Bachelor of Information Systems back in 2002, I worked developing sites in .NET before deciding to focus on UI &amp; Design. I went on to complete a Diploma in Digital Media at Media Design School, in the process receiving industry awards in both Design &amp; Development for my work.</p>                
+                <p>I have a keen eye for aesthetics &amp; usability something that shows in my work and also enjoy to researching to keep up with the latest methods &amp; technologies.</p>
+                <p>When I'm not crafting websites, I can be found on a found on a football ground or hiking in the wilderness.</p>            
+            </article>
+            </section>           
+
+<section class="wrap main">
+			<a name="contact" id="contact"></a>
+            <div class="breadcrumb"><h3>say hello</h3></div>
+            <div class="half">
+                    		<h1>I would like to hear about your opportunity</h1>
+                            <h3>Fill Out the contact form or contact me via the options below</h3>                                                 
+                     	    <h4><a aria-hidden="true" data-icon="&#xe00e;" href="mailto:me@sjmcpherson.com"> me@sjmcpherson.com</a></h4>
+                            <h4><a aria-hidden="true" data-icon="&#xe004;" href="tel:021-733996"> 021-733996</a></h4>
+                            <h4><a aria-hidden="true" data-icon="&#xe00d;" href="https://twitter.com/sjmcpherso"> @sjmcpherso</a></h4>
+                            <h4><a aria-hidden="true" data-icon="&#xe002;" href="nz.linkedin.com/in/stuartmcpherson"> LinkedIn Profile</a></h4>
+        	</div>                
+			<div class="form half">                               
+                               <h3>Contact Form</h3>
+                               <hr class="light" />
+
+
+                               <form id="cform" class="cform" method="post" action="process.php">
+                                <ul id="errors" class="forminfo">
+                                    <li id="info">There were some problems with your form submission:</li>
+
+                                </ul>
+                                <div id="success" class="forminfo">
+                                	<h3>Thank you for your message!</h3>
+                                    <h4>I will endeavor get back to you soon</h4>
+                               </div>
+                               <fieldset>
+                               <label for="name"><span>*</span>Name:</label><input type="name" value="" name="name" required="required" id="name" placeholder="your name" />                               
+                               <label for="email"><span>*</span>Email:</label><input type="email" value="" name="email" required="required" id="email" placeholder="your email" />
+                               <label for="phone">Phone:</label><input type="tel" name="phone" value="" id="phone" placeholder="your phone number" />
+                               <label for="business">Business:</label><input type="text" value="" name="business" id="business" placeholder="name of your business" />
+                               <label for="message"><span>*</span>Message:</label><textarea required="required" id="message" name="message" placeholder="your message"></textarea>
+                               <button type="submit" id="submit" class="button"  form="contact-form" value="send message">send message</button> 
+                               </fieldset>
+                               </form>
+           </div>
+           
+</section><!-- END MAIN --> 
+<footer>
+  <section class="wrap contact">
+  	    <h2>contact me</h2>
+  	    <ul>
+        <li><a aria-hidden="true" data-icon="&#xe00e;" href="mailto:me@sjmcpherson.com"> me@sjmcpherson.com</a></li>
+        <li><a aria-hidden="true" data-icon="&#xe004;" target="_blank" href="tel:021-733996"> 021-733996</a></li> 
+        <li><a aria-hidden="true" data-icon="&#xe00d;" target="_blank" href="https://twitter.com/sjmcpherso"> @sjmcpherso</a></li>
+ 		</ul>
+  </section>
+</footer>		
+</div>
+<!--/#inner-wrap-->
+</div>
+<!--/#outer-wrap-->
+  
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
+<!--[if lt IE 9]><script src="js/selectivizr-min.js?ver=1.0.2"></script><![endif]-->
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/jquery-scroll-link.js"></script>
+<script src="js/jquery-box-fader.js"></script>
+<script src="js/script.js"></script>     
+<script src="js/main.js"></script>    
+<script src="js/controller.js"></script>  
+        <script>		
+            var _gaq=[['_setAccount','UA-40796844-1'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src='//www.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));			
+        </script>   
+</body></html>
